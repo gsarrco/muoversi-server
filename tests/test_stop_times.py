ff7568625d118dac98c6d295ee48711904907be3
Giacomo Sarrocco <8850618+gsarrco@users.noreply.github.com>
2023-06-20 22:37:56 +0200

    Update test to reflect changes

--- tests/test_stop_times.py
+++ tests/test_stop_times.py
@@ -1,4 +1,4 @@
-from datetime import time, date, datetime, timedelta
+from datetime import time, date, datetime
 
 import pytest
 
@@ -16,8 +16,7 @@ def test_night_stop_times(db_file):
     day = date(2023, 2, 11)
     stop_times = db_file.get_stop_times('', start_time, dep_stop_ids, day, 0, 'P.LE ROMA')
 
-    day_after = day + timedelta(days=1)
-    check_start_dt = datetime.combine(day_after, time(0, 0))
-    check_end_dt = datetime.combine(day_after, time(4, 0))
+    check_start_dt = datetime.combine(day, time(0, 0))
+    check_end_dt = datetime.combine(day, time(4, 0))
     assert any(stop_time for stop_time in stop_times if
                check_start_dt < stop_time.dep_time < check_end_dt), 'no stop_times with dep_time before 4 am'
