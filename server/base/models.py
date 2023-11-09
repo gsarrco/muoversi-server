69f3e8d8967703ff466c33d41f761c148b2508a3
Giacomo Sarrocco <git@sarr.co>
2023-11-09 17:14:59 +0100

    Merge Trip into StopTime

--- server/base/models.py
+++ server/base/models.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 from typing import Optional
 
-from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
 
 Base = declarative_base()
@@ -42,32 +42,18 @@ class Stop(Base):
     stop_times = relationship('StopTime', back_populates='stop', cascade='all, delete-orphan')
     active: Mapped[bool] = mapped_column(server_default='true')
 
-
-class Trip(Base):
-    __tablename__ = 'trips'
+class StopTime(Base):
+    __tablename__ = 'stop_times'
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    stop_id: Mapped[str] = mapped_column(ForeignKey('stops.id'))
+    stop: Mapped[Stop] = relationship('Stop', back_populates='stop_times')
+    sched_arr_dt: Mapped[Optional[datetime]]
+    sched_dep_dt: Mapped[Optional[datetime]]
+    platform: Mapped[Optional[str]]
     orig_id: Mapped[str]
     dest_text: Mapped[str]
     number: Mapped[int]
     orig_dep_date: Mapped[date]
     route_name: Mapped[str]
     source: Mapped[str] = mapped_column(server_default='treni')
-    stop_times = relationship('StopTime', back_populates='trip', cascade='all, delete-orphan', passive_deletes=True)
-
-    __table_args__ = (UniqueConstraint('source', 'number', 'orig_dep_date'),)
-
-
-class StopTime(Base):
-    __tablename__ = 'stop_times'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    trip_id: Mapped[int] = mapped_column(ForeignKey('trips.id', ondelete='CASCADE'))
-    trip: Mapped[Trip] = relationship('Trip', back_populates='stop_times')
-    stop_id: Mapped[str] = mapped_column(ForeignKey('stops.id'))
-    stop: Mapped[Stop] = relationship('Stop', back_populates='stop_times')
-    sched_arr_dt: Mapped[Optional[datetime]]
-    sched_dep_dt: Mapped[Optional[datetime]]
-    platform: Mapped[Optional[str]]
-
-    __table_args__ = (UniqueConstraint('trip_id', 'stop_id'),)
