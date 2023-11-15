1f5e7215c31a292fb2d1f721d3390c7d4f1dd083
Giacomo Sarrocco <git@sarr.co>
2023-11-15 18:17:58 +0100

    Separate attachment and detachment of partitions in a new file

--- save_data.py
+++ save_data.py
@@ -1,10 +1,6 @@
 import logging
-from datetime import date, timedelta
 
-from sqlalchemy import inspect, text
-
-from server.base.models import StopTime
-from server.sources import engine, session, sources
+from server.sources import session, sources
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -14,30 +10,6 @@ logger = logging.getLogger(__name__)
 
 def run():
     session.commit()
-
-    today = date.today()
-
-    def part_name(day: date):
-        return f'stop_times_{day.strftime("%Y%m%d")}'
-
-    for i in range(3):
-        day: date = today + timedelta(days=i)
-        day_after: date = day + timedelta(days=1)
-        partition_name = part_name(day)
-        if not inspect(engine).has_table(partition_name):
-            session.execute(text(f"CREATE TABLE {partition_name} PARTITION OF stop_times FOR VALUES FROM ('{day}') TO ('{day_after}')"))
-            session.commit()
-    # start from the day before yesterday for detaching partitions
-    i = 2
-    while True:
-        day = today - timedelta(days=i)
-        try:
-            session.execute(text(f'ALTER TABLE stop_times DETACH PARTITION {part_name(day)}'))
-            session.commit()
-        except:
-            session.rollback()
-            break
-        i += 1
 
     for source in sources.values():
         try:
