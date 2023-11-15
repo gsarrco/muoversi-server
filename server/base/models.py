1703143bc248745ca5ee690a60180b093d3b74e5
Giacomo Sarrocco <git@sarr.co>
2023-11-15 15:16:14 +0100

    Use simple StopTime db model without  custom DeclarativeMeta

--- server/base/models.py
+++ server/base/models.py
@@ -45,56 +45,9 @@ class Stop(Base):
     active: Mapped[bool] = mapped_column(server_default='true')
 
 
-class PartitionByOrigDepDateMeta(DeclarativeMeta):
-    def __new__(cls, clsname, bases, attrs, *, partition_by):
-        @classmethod
-        def get_partition_name(cls_, key):
-            return f'{cls_.__tablename__}_{key}'
+class StopTime(Base):
+    __tablename__ = 'stop_times'
 
-        @classmethod
-        def create_partition(cls_, day: date):
-            key = day.strftime('%Y%m%d')
-            if key not in cls_.partitions:
-                Partition = type(
-                    f'{clsname}{key}',
-                    bases,
-                    {'__tablename__': cls_.get_partition_name(key)}
-                )
-
-                Partition.__table__.add_is_dependent_on(cls_.__table__)
-
-                day_plus_one = day + timedelta(days=1)
-                event.listen(
-                    Partition.__table__,
-                    'after_create',
-                    DDL(
-                        f"""
-                        ALTER TABLE {cls_.__tablename__}
-                        ATTACH PARTITION {Partition.__tablename__}
-                        FOR VALUES FROM ('{day}') TO ('{day_plus_one}')
-                        """
-                    )
-                )
-
-                cls_.partitions[key] = Partition
-
-            return cls_.partitions[key]
-
-        attrs.update(
-            {
-                '__table_args__': attrs.get('__table_args__', ())
-                                  + (dict(postgresql_partition_by=f'RANGE({partition_by})'),),
-                'partitions': {},
-                'partitioned_by': partition_by,
-                'get_partition_name': get_partition_name,
-                'create_partition': create_partition
-            }
-        )
-
-        return super().__new__(cls, clsname, bases, attrs)
-
-
-class StopTimeMixin:
     id: Mapped[int] = mapped_column(primary_key=True)
     sched_arr_dt: Mapped[Optional[datetime]]
     sched_dep_dt: Mapped[Optional[datetime]]
@@ -105,17 +58,7 @@ class StopTimeMixin:
     number: Mapped[int]
     route_name: Mapped[str]
     source: Mapped[str] = mapped_column(server_default='treni')
-
-    @declared_attr
-    def stop_id(self) -> Mapped[str]:
-        return mapped_column(ForeignKey('stops.id'))
-
-    @declared_attr
-    def stop(self) -> Mapped[Stop]:
-        return relationship('Stop', foreign_keys=self.stop_id)
-
-
-class StopTime(StopTimeMixin, Base, metaclass=PartitionByOrigDepDateMeta, partition_by='orig_dep_date'):
-    __tablename__ = 'stop_times'
-
+    stop_id: Mapped[str] = mapped_column(ForeignKey('stops.id'))
+    stop: Mapped[Stop] = relationship('Stop', foreign_keys=stop_id)
+    
     __table_args__ = (UniqueConstraint("stop_id", "number", "source", "orig_dep_date"),)
